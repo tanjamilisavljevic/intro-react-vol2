4327d51b92a031a97ec6bce68e566751308dbc6b
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-01 20:10:40 +0200

    Map list items and display them

--- src/todoList.js
+++ src/todoList.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
 export default function TodoList(props) {
+    const todos = ["Don't procrastinate!", " Do work!"];
     return (
         <div>
             <h1> My TO-DO List</h1>
             <ul>
-                <li>
-                    <input type="checkbox" /> Don't procrastinate!
-                </li>
-                <li>
-                    <input type="checkbox" /> Do work!
-                </li>
+                {todos.map((todo) => {
+                   return <li key={todo}> <input type="checkbox"/> {todo} </li>
+                })
+                }
             </ul>
-
         </div>
     );
 }
