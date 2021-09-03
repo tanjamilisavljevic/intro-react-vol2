f30aeacf6ff6b403b258aca23248865fa23cf1fd
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-03 15:50:18 +0200

    Add functionality to delete selected todos

--- src/form.js
+++ src/form.js
@@ -1,5 +1,11 @@
 import React, {useRef, useState, useEffect} from 'react';
 import TodoList from "./todoList";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 const LOCAL_STORAGE_KEY = "TodoList.todos";
 
@@ -23,7 +29,23 @@ export default function Form() {
 
     function clickHandler() {
         const inputElement = inputRef.current.value;
-        setTodos([...todos, {name: inputElement, checked: 'false'}])
+        setTodos([...todos, {name: inputElement, checked: false}])
+    }
+
+    function clickHandlerDelete() {
+        const unCheckedTodos = todos.filter(todo => todo.checked === false);
+        console.log(unCheckedTodos);
+        setTodos(unCheckedTodos)
+    }
+    function checkTodoHandler(todoName) {
+        const newTodos =  [...todos];
+        const checkedTodo = newTodos.find(todo => todo.name === todoName);
+        if (checkedTodo.checked) {
+            checkedTodo.checked = false;
+        } else {
+            checkedTodo.checked = true;
+        }
+        setTodos(newTodos);
     }
 
     useEffect(() => {
@@ -37,14 +59,16 @@ export default function Form() {
                 <h1> My TO-DO List</h1>
             </div>
             <div className="contentWrapper">
-                < TodoList todos={todos}/>
+                < TodoList todos={todos} checkTodo={checkTodoHandler}/>
 
                 <label htmlFor="newToDo"/>
-                <input ref={inputRef} class="newTodo" type="text" name="newToDo" id="newToDo"
+                <input ref={inputRef} className="newTodo" type="text" name="newToDo" id="newToDo"
                        placeholder="Do the groceries..."/>
                 <br/>
-                <input class="submit" type="submit" id="submit" name="submit" value="Add to list"
+                <input className="submit" type="submit" id="submit" name="submit" value="Add to list"
                        onClick={clickHandler}/>
+                <input className="submit" type="submit" id="delete" name="delete" value="Delete selected"
+                       onClick={clickHandlerDelete}/>
             </div>
         </>
     );
--- src/todoList.js
+++ src/todoList.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function TodoList(props) {
 
+
     const todos = props.todos;
+    const checkTodo = props.checkTodo;
+
 
     return (
         <div>
             <ol>
                 {todos.map((todo) => {
-                    return <li key={todo.name}><input type="checkbox" checked={todos.checked}/> {todo.name} </li>
+                    return <li key={todo.name}><input type="checkbox" checked={todo.checked} onChange={() => checkTodo(todo.name)}/> {todo.name} </li>
                 })
                 }
             </ol>
